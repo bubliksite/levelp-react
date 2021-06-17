4344957ce479c017f7d39b11ad3d81ab3625a866
bubliksite <serj.solntsev@gmail.com>
2021-06-17 14:34:12 +0300

    Lesson 5 (Homework, закомментирован propType title кнопки Button, различные типы возможны. CategoryList переделан на "умный/глупый" компоненты)

--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -1,54 +1,22 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import './style.scss'
-import {useDispatch, useSelector} from 'react-redux'
-import {
-  actionCreateCategory,
-  actionDeleteCategory
-} from '../../store/categories'
 import Input from '../Input'
 import Button from '../Button'
-import {ListGroup, ListGroupItem} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
-import {Trash} from 'react-bootstrap-icons'
-import Spinner from '../Spinner'
 import Alert from '../Alert'
+import Spinner from '../Spinner'
+import {ListGroup, ListGroupItem} from 'react-bootstrap'
+import {Trash} from 'react-bootstrap-icons'
+import {Link} from 'react-router-dom'
 
-const CategoryList = () => {
-  const categories = useSelector((state) => state.category)
-  const dispatch = useDispatch()
-  const [change, setChange] = useState('')
-  const [alert, setAlert] = useState(false)
-  const [showSpinner, setShowSpinner] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => setShowSpinner(false), 5000)
-  })
-
-  const handlerChangeInput = (e) => {
-    setAlert(false)
-    setChange(e.target.value)
-  }
-  const handlerAddCategory = (e) => {
-    e.preventDefault()
-    if (change) {
-      const data = {
-        id: Date.now(),
-        title: change
-      }
-      dispatch(actionCreateCategory(data))
-      setChange('')
-    } else {
-      setAlert(true)
-    }
-  }
-
-  const handlerDeleteCategory = (id) => {
-    dispatch(actionDeleteCategory(id))
-    if (categories.category.length === 1) {
-      setShowSpinner(false)
-    }
-  }
-
+const CategoryList = ({
+  categories,
+  alert,
+  change,
+  showSpinner,
+  handlerChangeInput,
+  handlerAddCategory,
+  handlerDeleteCategory
+}) => {
   return (
     <>
       <form
