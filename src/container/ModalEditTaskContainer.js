faeeb446a5c26ef25f49ec6d4cc2a4199d01abd4
bubliksite <serj.solntsev@gmail.com>
2021-07-07 18:13:11 +0300

    Lesson 9 (01:51)

--- src/container/ModalEditTaskContainer.js
+++ src/container/ModalEditTaskContainer.js
@@ -3,9 +3,11 @@ import ModalEditTask from '../components/Modals/ModalEditTask'
 import {useDispatch, useSelector} from 'react-redux'
 import {actionHideModal} from '../store/modals'
 import {getTodo} from '../store/todos/selectors'
+import {actionEditTodo} from '../store/todos'
 
 export default function ModalEditTaskContainer({id}) {
   const {task} = useSelector((state) => getTodo(state))
+  const dispatch = useDispatch()
   const title = task.find((item) => item.id === id).title
   const [value, setValue] = useState(title)
 
@@ -13,14 +15,25 @@ export default function ModalEditTaskContainer({id}) {
     setValue(e.target.value)
   }
 
-  const dispatch = useDispatch()
+  const handlerEditTask = () => {
+    const data = {
+      title: value,
+      id
+    }
+    dispatch(actionEditTodo(data))
+    console.log(task)
+    handlerHideModal()
+  }
+
   const handlerHideModal = () => {
     dispatch(actionHideModal())
   }
+
   return (
     <ModalEditTask
       handlerHideModal={handlerHideModal}
       handlerChangeInput={handlerChangeInput}
+      handlerEditTask={handlerEditTask}
       title={title}
       value={value}
     />
